cbd16c294ecbbcfca7933954d0f1ebfcb075417f
José Yan <54294970+YanLipu@users.noreply.github.com>
2024-10-14 18:28:03 -0300

    chore: setup project for build to production

--- src/test/java/com/gerenciamento_medico/medico_api/MedicoApiApplicationTests.java
+++ src/test/java/com/gerenciamento_medico/medico_api/MedicoApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.gerenciamento_medico.medico_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MedicoApiApplicationTests {
 
 	@Test
