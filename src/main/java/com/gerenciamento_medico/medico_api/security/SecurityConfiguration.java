d69628a0554e666bb7648c8dcab25edd94847b7f
José Yan <54294970+YanLipu@users.noreply.github.com>
2024-10-15 19:28:01 -0300

    feat: setup cors for local browser requests

--- src/main/java/com/gerenciamento_medico/medico_api/security/SecurityConfiguration.java
+++ src/main/java/com/gerenciamento_medico/medico_api/security/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +26,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecutiry) throws Exception {
         return httpSecutiry
+                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
                 .csrf(csrf->csrf.disable())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize->authorize
